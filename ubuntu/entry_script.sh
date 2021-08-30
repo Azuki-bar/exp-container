8f5ce239b80d03d6d1866ddcc322b8ea6ed12ce0
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2021-08-30 17:48:25 +0900

    change init script download URL

--- ubuntu/entry_script.sh
+++ ubuntu/entry_script.sh
@@ -1,4 +1,4 @@
 #!bin/env bash
 
 echo "alias python='python3'" >> ~/.bashrc
-curl -L https://azukibar.dev/init |bash >/root/log_init 2>/root/log_err
+curl -L 'https://raw.githubusercontent.com/Azuki-bar/dotfiles/master/init.sh' |bash >/root/log_init 2>/root/log_err
